fe24944e351577c4d87d50e4c68ba29becbf455b
safronman <safronmanbox@gmail.com>
2025-04-18 16:13:53 +0300

    add cors - 4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import express, { type Application } from "express"
+import { corsOptions } from "./common/corsOptions"
 import authRoutes from "./routes/auth"
 import smoothiesRoutes from "./routes/smoothies"
 import cors from "cors"
@@ -9,28 +10,7 @@ dotenv.config()
 const app: Application = express()
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://smoothies-express-supabase.onrender.com",
-]
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Разрешаем запросы без origin (например, Postman или curl)
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true)
-      } else {
-        return callback(new Error("Not allowed by CORS"))
-      }
-    },
-    credentials: true,
-  }),
-)
+app.use(cors(corsOptions))
 app.use(express.json())
 
 // Routes
---
+++ src/common/corsOptions.ts
@@ -0,0 +1,21 @@
+import { CorsOptions } from "cors"
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://smoothies-express-supabase.onrender.com",
+]
+
+export const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}

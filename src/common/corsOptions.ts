b805fd9bf5a52441fad3342e497ce6ba54c1a06e
safronman <safronmanbox@gmail.com>
2025-05-13 11:27:03 +0300

    add smoothies-express-supabase.vercel.app to CORS

--- src/common/corsOptions.ts
+++ src/common/corsOptions.ts
@@ -6,6 +6,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://smoothies-express-supabase.onrender.com",
+  "https://smoothies-express-supabase.vercel.app",
 ]
 
 export const corsOptions: CorsOptions = {

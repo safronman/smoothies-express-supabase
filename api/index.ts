762752b46b28d2d9c71fbcf9d4d7fc19a741a545
safronman <safronmanbox@gmail.com>
2025-05-13 16:35:03 +0300

    add serverless

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import authRoutes from '../src/routes/auth'
+import smoothiesRoutes from '../src/routes/smoothies'
+import healthRoutes from '../src/routes/health'
+import cors from 'cors'
+import { corsOptions } from '../src/common/corsOptions'
+import { setupSwagger } from '../src/docs/swagger'
+
+const app = express()
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+setupSwagger(app)
+
+app.use('/api', authRoutes)
+app.use('/api', smoothiesRoutes)
+app.use('/api', healthRoutes)
+
+export default app
